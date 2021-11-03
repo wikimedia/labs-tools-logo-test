f061a50e3560f47a2fef9018d057ad790fb72167
Kunal Mehta <legoktm@debian.org>
2021-11-02 22:47:41 -0700

    Update my email address
    Change-Id: I683752c23ac68c41d04e6370a016ac025e5c4b3d

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 /*
 Easily test new logos on Wikimedia sites
-Copyright (C) 2021 Kunal Mehta <legoktm@member.fsf.org>
+Copyright (C) 2021 Kunal Mehta <legoktm@debian.org>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
